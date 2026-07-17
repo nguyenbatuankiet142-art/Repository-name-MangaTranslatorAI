2196a49e973219b2c80dccad1653a3fa3839bc66
nguyenbatuankiet142-art <nguyenbatuankiet142@gmail.com>
2026-07-17 11:06:54 +0700

    Create add.py

---
+++ add.py
@@ -0,0 +1,12 @@
+from config.config import *
+
+def main():
+
+    print("="*40)
+    print(" MangaTranslatorAI ")
+    print("="*40)
+
+    print("Ready.")
+
+if __name__ == "__main__":
+    main()
